8320425c20b8b876de77823f18de95361104f5eb
DucLM177 <laiminhduc99@gmail.com>
2020-05-26 17:24:44 +0700

    1

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -1,12 +1,17 @@
 <?php
 require_once 'Model/User.php'; 
-
 class UserController
 {   
     public function get_All_Users()
     {
+    
         $userModel = new User();
         $userList =$userModel->get_All_Data();
+    }
+
+    public function hello()
+    {
+        echo "dung roi do";
     }
 }
 ?>
--- index.php
+++ index.php
@@ -9,9 +9,12 @@ $conn->connect();
 
 if (isset($_GET['controller'], $_GET['action']))
 {
-    $controller = $_GET['controller'];
+    $controller_url = $_GET['controller'];
     $action =  $_GET['action'];
+    require_once "Controller/".$controller_url.".php";
+    $controller = new $controller_url;
     $controller->$action();
+
 }
 else require_once('Views/home.php');
 
