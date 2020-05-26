2b0ff7fc8262f233063c8702a75db1fcf76493e4
DucLM177 <laiminhduc99@gmail.com>
2020-05-26 17:28:55 +0700

    1

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 <?php
 require_once 'Model/User.php'; 
 class UserController
@@ -10,14 +10,4 @@ class UserController
         $userList =$userModel->get_All_Data();
     }
 
-    public function hello()
-    {
-        echo "dung roi do";
-    }
-=======
-<?php 
-class UserController {
-    
->>>>>>> 78e7ef9d0fbc98f06d536f416c9862ade0cce93a
-}
 ?>
--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <?php 
 session_start();
-<<<<<<< HEAD
-
 //connect DB
 require_once 'Model/BaseConnect.php';
 $conn = new BaseConnect();
@@ -18,14 +16,4 @@ if (isset($_GET['controller'], $_GET['action']))
 
 }
 else require_once('Views/home.php');
-
-=======
-if (isset($_GET['controller'], $_GET['action']))
-{
-    $controller = $_GET['controller'];
-    $action =  $_GET['action'];
-    $controller->$action;
-}
-else require_once('view/home.php');
->>>>>>> 78e7ef9d0fbc98f06d536f416c9862ade0cce93a
 ?>
